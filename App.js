67c4e7169cd37439c57d7b90c4d474cfc8f042ff
gopialuri24 <gopialuri24@outlook.com>
2024-01-26 01:15:17 +0530

    second

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const heading = document.createElement("h2")
-// heading.innerHTML="HI"
-// const root = document.getElementById("root");
-// root.appendChild(heading)
+const jsxheading = (<h1 id="">
+    Hello React from JSX!
+    </h1>);
 
-const heading = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "Hello ReactJS"),
-        React.createElement("h2", {}, "Hello ReactJS")]),
-
-    React.createElement("div", { id: "child2" },
-        [React.createElement("h1", {}, "Hello ReactJS"),
-        React.createElement("h2", {}, "Hello ReactJS")])])
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
 
-
-
+root.render(jsxheading);
